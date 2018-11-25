9a51831b5e2ac3749fc17f6f534ba864aa06ecb3
navh <navarre.hebb@gmail.com>
2018-11-24 19:18:13 -0500

    Tried to make some beautiful C files

---
+++ markerDetection/cmake-build-debug/CMakeFiles/markerDetection.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/markerDetection.dir/main.cpp.obj"
+  "markerDetection.pdb"
+  "markerDetection.exe"
+  "markerDetection.exe.manifest"
+  "markerDetection.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/markerDetection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ markerDetection/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/aruco.hpp>
+#include <opencv2/highgui.hpp>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
