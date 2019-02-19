10b4cf63430e5a1cfcd777b7f9b4b9bd559e54b0
navh <navarre.hebb@gmail.com>
2019-02-18 21:30:11 -0500

    Day one of the adventure week

--- catkin_ws/src/aruco_tracker/src/aruco_tracker.py
+++ catkin_ws/src/aruco_tracker/src/aruco_tracker.py
@@ -1,6 +1,5 @@
 #! /usr/bin/env python
 
-
 #python libs
 import sys, time
 
@@ -8,8 +7,11 @@ import sys, time
 import numpy as np
 from scipy.ndimage import filters
 
+from matplotlib import pyplot as plt
+
 #openCV
 import cv2
+import cv2.aruco as aruco
 
 #ros libraries
 import roslib
@@ -17,40 +19,129 @@ import rospy
 
 #ros messages
 from sensor_msgs.msg import CompressedImage
+from geometry_msgs.msg import Twist
 
+#some nasty globals
+font = cv2.FONT_HERSHEY_SIMPLEX
+versionString = 'v0.1-'
 VERBOSE=True
+MOVEMENT_ENABLED=True
+LeftTurn = 0.05
+RightTurn = 0.05
+RobotSpeed = 0.05
+NoMarkerTurn = 0.3
 
-class image_feature:
+
+class aruco_tracker:
 
     def __init__(self):
+
+        # Initiate FAST object with default values
+        self.fast = cv2.FastFeatureDetector_create(184)
+
+        #This is where we will store the most recent depth array
+        self.depth_arr = None
+
+        #This is for how we steeerererererer
+        self.xVelocity = 0
+        self.zRotation = 0
+
         #this is where we publish the output
-        self.image_pub = rospy.Publisher("/output/image_raw/compressed", CompressedImage)
+        self.image_pub = rospy.Publisher("/output/image_raw/compressed", CompressedImage, queue_size = 1)
+        self.depth_pub = rospy.Publisher("/output/depth/compressed", CompressedImage, queue_size = 1)
+        self.velo_pub = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size = 10)
 
         #this is where we read from the robot
-        self.subscriber = rospy.Subscriber("/camera/rgb/image_raw/compressed",CompressedImage, self.callback, queue_size=1)
+        camera_channel = '/camera/rgb/image_raw/compressed'
+        depth_channel = '/camera/depth/image_raw/compressedDepth'
+        self.subscriber = rospy.Subscriber(camera_channel,CompressedImage, self.callback, queue_size=1)
+        self.depth_subscriber = rospy.Subscriber(depth_channel, CompressedImage, self.depthcallback, queue_size=1)
         if VERBOSE:
-            print "subscribed to /camera/image/compressed"
+            print 'subscribed to ' + camera_channel
+            print 'subscribed to ' + depth_channel
+
+    def depthcallback(self, ros_data):
+        lengthOfHeader = 12
+        raw_data = ros_data.data[lengthOfHeader:]
+        np_arr = np.fromstring(raw_data, np.uint8)
+        self.depth_arr = cv2.imdecode(np_arr,cv2.IMREAD_ANYDEPTH)
+        normalizedImg = None
+        normalizedImg = cv2.normalize(self.depth_arr,normalizedImg,0, 255, cv2.NORM_MINMAX,cv2.CV_8UC3)
+        histogram = cv2.applyColorMap(normalizedImg,cv2.COLORMAP_RAINBOW)
+
+        if VERBOSE:
+            print 'received depth of type: "%s"' % ros_data.format
+
+        # create a compressed image
+        msg = CompressedImage()
+        msg.header.stamp = rospy.Time.now()
+        msg.format = 'jpeg'
+        msg.data = np.array(cv2.imencode('.jpg', histogram)[1]).tostring()
+        # publish that image
+        self.depth_pub.publish(msg)
 
     def callback(self, ros_data):
+
+        self.xVelocity = 0
+        self.zRotation = 0
+        outputText = versionString
+
         if VERBOSE:
             print 'received image of type: "%s"' % ros_data.format
 
         np_arr = np.fromstring(ros_data.data, np.uint8)
         image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR) #second argument should be cv2.IMREAD_COLOR for OpenCV >= 3.0
 
-        method = "GridFAST"
-        feat_det = cv2.FeatureDetector_create(method)
-        time1 = time.time()
+        gray = cv2.cvtColor(image_np, cv2.COLOR_BGR2GRAY)
+        #red square detection, this should eventually be aruco markers
 
-        featPoints = feat_det.detect(cv2.cvtColor(image_np, cv2.COLOR_BGR2GRAY))
-        time2 = time.time()
+        #hsv_red = cv2.cvtColor(image_np, cv2.COLOR_BGR2HSV)
+        #lower_blue = np.array([0,100,100])
+        #upper_blue = np.array([10,255,255])
 
-        if VERBOSE:
-            print "%s detector found: %s points in: %s sec."%(method,len(featPoints),(time2-time1))
+        #mask = cv2.inRange(hsv_red,lower_blue,upper_blue)
 
-        for point in featPoints:
-            x,y = point.pt
-            cv2.circle(image_np,(int(x),int(y)), 3, (0,0,255), -1)
+        #n_white_pix = np.sum(mask == 255)
+
+        #aruco marker detection
+        aruco_dict = aruco.Dictionary_get(aruco.DICT_ARUCO_ORIGINAL)
+        parameters = aruco.DetectorParameters_create()
+
+        corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+
+        # find and draw the keypoints
+        kp = self.fast.detect(gray)
+
+        for p in kp:
+            x,y = p.pt
+            x = int(x)
+            y = int(y)
+            distance = self.depth_arr[y][x]
+            cv2.putText(image_np, (str(distance) + 'mm'), (x, y), font, 1, (0, 0, 180), 2, cv2.LINE_AA)
+            cv2.circle(image_np,(x,y),1,(0,0,255),5)
+
+        if np.all(ids != None):
+
+            aruco.drawDetectedMarkers(image_np,corners)
+
+            strg = ''
+            for i in range(0,ids.size):
+                strg += str(ids[i][0])+', '
+                cX = int((corners[i][0][0][0] + corners[i][0][1][0]) / 2)
+                cY = int((corners[i][0][0][1] + corners[i][0][2][1]) / 2)
+                distance = self.depth_arr[cY][cX]
+                cv2.putText(image_np, (str(distance) + 'mm'), (cX, cY), font, 1, (0, 255, 255), 2, cv2.LINE_AA)
+
+            height, width = image_np.shape[:2]
+            cX = int((corners[0][0][0][0] + corners[0][0][1][0]) / 2)
+            outputText += 'spotted: ' + strg
+
+        else:
+            outputText += 'No Marker'
+
+        self.zRotation -= NoMarkerTurn
+
+        cv2.putText(image_np, (outputText), (32, 32), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
         #create a compressed image
         msg = CompressedImage()
@@ -60,9 +151,19 @@ class image_feature:
         #publish that image
         self.image_pub.publish(msg)
 
+        #create a movement message
+        move_cmd = Twist()
+        if MOVEMENT_ENABLED:
+            move_cmd.linear.x = self.xVelocity
+            move_cmd.angular.z = self.zRotation
+        else:
+            move_cmd.linear.x = 0
+            move_cmd.angular.z = 0
+        self.velo_pub.publish(move_cmd)
+
 def main(args):
-    ic = image_feature()
-    rospy.init_node('image_feature', anonymous=True)
+    ac = aruco_tracker()
+    rospy.init_node('aruco_tracker', anonymous=True)
     try:
         rospy.spin()
     except KeyboardInterrupt:
